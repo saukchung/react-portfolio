870ce94aa746448907415a09d10f4a9c3670445c
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-21 19:50:47 -0400

    Styled the nav, and rearranged the nav(I think)

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import './nav.css';
 
-export default function Nav() {
+export default function Nav({currentPage, handlePageChange}) {
     return (
-        <nav>
-        <h1>Sa Uk Chung</h1>
-        <div>
-          <a href="#">About Me</a>
-        </div>
-        <div>
-          <a href="#">Portfolio</a>
-        </div>
-        <div>
-          <a href="#">Contact</a>
-        </div>
-        <div>
-          <a href="#">Resume</a>
-        </div>
-        </nav>
+    <div className='header'>
+      <h1>Brian Chung</h1>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange('About')}
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#portfolio"
+            onClick={() => handlePageChange('Portfolio')}
+            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+          >
+            Portfolio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#resume"
+            onClick={() => handlePageChange('Resume')}
+            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+    </div>
     )
-}
+};
