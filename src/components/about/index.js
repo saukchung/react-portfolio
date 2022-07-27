f41273ba335bcc700b5bb269e46fc4504211d11b
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-27 01:53:26 -0400

    LAyout is working

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import './about.css';
 import pic from './ballin.gif';
 
 export default function About() {
+    const styles = {
+        aboutContainer: {
+            display: "flex",
+        },
+        aboutImage: {
+            flex: "1"
+        },
+        aboutDiv: {
+            flex: "1"
+        }
+    }
     return (
-        <>
-            <img src={pic} alt=""></img>
-            <p>THIS IS SUPPOSED TO BE ABOUT ME</p>
-        </>
+        <div style={styles.aboutContainer}>
+            <img src={pic} alt="a .gif of a game of NBA" style={styles.aboutImage}></img>
+            <div style={styles.aboutDiv}>
+                <h2>Who am I?</h2>
+                <p>THIS IS SUPPOSED TO BE ABOUT ME</p>
+            </div>
+        </div>
     )
 }
